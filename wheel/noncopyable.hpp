2d72a01a558734363bd524b63ae7dcfb9e9ce3d0
霍红卫（Huo, Hongwei） <hwhuo@mail.xidian.edu.cn>
2023-06-13 16:53:43 +0800

    Add files via upload

---
+++ wheel/noncopyable.hpp
@@ -0,0 +1,21 @@
+/*============================================
+# Filename: ???
+# Ver:   Date: 2023-05-XX
+# Copyright (C)  Zongtao He, Hongwei Huo, and Jeffrey S. Vitter
+#
+This program is free software; you can redistribute it and/or modify it 
+under the terms of the GNU Lesser General Public License as published 
+by the Free  Software Foundation.
+#
+# Description: ???
+=============================================*/
+#pragma once
+class noncopyable
+{
+protected:
+    noncopyable() {}
+    ~noncopyable() {}
+private:
+    noncopyable(const noncopyable&);
+    const noncopyable& operator=(const noncopyable&);
+};
